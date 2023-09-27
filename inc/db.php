9c1380b540b2fdfe2a22a27a1997b3a854174349
hussam-159753 <146278351+hussam-159753@users.noreply.github.com>
2023-09-28 00:36:00 +0300

    Add files via upload

---
+++ inc/db.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect('localhost','root','root','win');
+
+if(!$conn){
+echo 'error:' . mysqli_connect_error();
+}
+?>
---
+++ parts/footer.php
@@ -0,0 +1,11 @@
+
+
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/loader.js"></script>
+    <script src="./js/script.js"></script>
+    
+
+</body>
+</html>
+
+
